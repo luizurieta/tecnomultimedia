6c8ee362b97987b16f155b7dcf9bd19398058549
luizurieta <133244065+luizurieta@users.noreply.github.com>
2023-11-19 03:36:01 -0300

    tp5.js

--- tp5/tp5.js
+++ tp5/tp5.js
@@ -1,4 +1,10 @@
 let juego;
+let imagen = [];
+function preload() {
+  for (let i = 0; i < 4; i++) {
+    imagen[i] = loadImage("data/imagen" + i + ".jpg");
+  }
+}
 function setup() {
 createCanvas (400,400);
 juego = new Juego ();
