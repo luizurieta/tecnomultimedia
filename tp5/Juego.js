2aa6ed90d13f00bc6d46ad3c62b510f3898ccce1
luizurieta <133244065+luizurieta@users.noreply.github.com>
2023-11-19 03:34:00 -0300

    Juego.js

--- tp5/Juego.js
+++ tp5/Juego.js
@@ -4,73 +4,119 @@ class Juego {
     this.personaje = new Personaje();
     this.estado = '0';
     this.diarios = [
-    new Diario (3,1,30),
-    new Diario (5,11,30),
-    new Diario (9,3,30),
-    new Diario (5,0,30),
-    new Diario (9,9,30),
-
+      new Diario (3, 1, 30),
+      new Diario (5, 11, 30),
+      new Diario (9, 3, 30),
+      new Diario (5, 0, 30),
+      new Diario (9, 9, 30),
     ];
+    this.textos = [
+      "LA FERIA DE LAS TINIEBLAS", "PRESS ENTER TO START", "WILL NECESITA ENCONTRAR", "LOS ARCHIVOS PARA SABER QUE ", "SECRETOS ESCONDE EL CIRCO", "CON LAS FLECHAS WILL COMENZARA A MOVERSE", "PERO DEBE APURARSE Y RECOLECTAR TODOS LOS ", "DIARIOS EN EL TIEMPO QUE TIENE ", "NO HAS LOGRADO EL OBJETIVO", "WILL AGRADECE TU AYUDA", "HAS VENCIDO", "WILL Y SUS AMIGOS LIBERARON"," SU CIUDAD", "PRESS ENTER TO REINICIAR"
+    ];
+      this.tiempoInicio = millis();
+
   }
   dibujar() {
+
     if (this.estado === '0') { //inicio
-      background (0);
-      fill (100, 255, 100);
+
+      image(imagen[0], 0, 0, 400, 400);
+
+      fill (255);
       textSize (28);
       textAlign (CENTER, CENTER);
-      text ("LA FERIA DE LAS TINIEBLAS", width /2, 100);
+      text (this.textos[0], width /2, 300);
       fill (255)
         textSize (18);
-      text ("PRESS ENTER TO START", 250, 380);
+      text (this.textos[1], 250, 380);
     } else if (this.estado ==='1') { //instrucciones
-      background (0);
-      fill (100, 255, 100);
+      noStroke();
+      image(imagen[1], 0, 0, 400, 400);
+      fill (0, 100);
+      rect (20, 20, 360, 360);
+      fill (255);
       textSize (20);
       textAlign (LEFT, CENTER);
-      text ("WILL NECESITA ENCONTRAR", 50, 100);
-      text ("LOS ARCHIVOS PARA SABER QUE ", 30, 140);
-      text ("SECRETOS ESCONDE EL CIRCO", 40, 180);
+      text (this.textos[2], 50, 100);
+      text (this.textos[3], 30, 140);
+      text (this.textos[4], 40, 180);
       textSize (14);
-      text ("CON LAS FLECHAS WILL COMENZARA A MOVERSE", 30, 220);
-      text ("PERO DEBE APURARSE Y RECOLECTAR TODOS LOS ", 5, 240);
-      text ("DIARIOS EN EL TIEMPO QUE TIENE ", 70, 260);
+      text (this.textos [5], 30, 220);
+      text (this.textos[6], 18, 240);
+      text (this.textos[7], 70, 260);
       fill (255)
         textSize (12);
-      text ("PRESS ENTER TO START", 250, 380);
+      text (this.textos [1], 250, 380);
     } else if (this.estado === '2') { //juego
       this.laberinto.dibujar();
       this.personaje.mostrar ();
-       for (let i = 0; i < this.diarios.length; i++) {
+      let todasRecolectadas = true;
+      for (let i = this.diarios.length - 1; i >= 0; i--) {
         const diario = this.diarios[i];
         if (!diario.recolectado) {
           diario.mostrar();
-          if (diario.colision(juego.personaje)) {
+          if (diario.colision(this.personaje.x * this.laberinto.tam, this.personaje.y * this.laberinto.tam, this.personaje.tam)) {
             diario.recolectado = true;
-               this.diarios.splice(i, 1);
+            this.diarios.splice(i, 1);
+          } else {
+                      todasRecolectadas = false;
+
           }
         }
-       }
-          } else if (this.estado === '3') { //ganaste
-       background (0);
-      fill (100, 255, 100);
-      textSize (20);
-      text ('HAS LOGRADO EL OBJETIVO',50,100)
-      text ('GRACIAS POR AYUDAR A WILL', 50, 140);
-      text ('AHORA SABE COMO ENFRENTAR AL', 20,180);
-      text ( 'ENEMIGO', 150,220);
-      textSize (12);
-      text ("PRESS ENTER TO REINICIAR", 220, 380);
-    } else if (this.estado === '4') { //perdiste
-         background (0);
-      fill (100, 255, 100);
-      textSize (20);
-      text ('NO HAS LOGRADO EL OBJETIVO', 50, 100);
-       text ('WILL AGRADECE TU AYUDA', 70, 140);
-        textSize (12);
-      text ("PRESS ENTER TO REINICIAR", 220, 380);
-  }
-  }
+      }
+      // Si no, continúa mostrando el juego
+      fill(255);
+      text('TIEMPO: ' + floor((millis() - this.tiempoInicio) / 1000) + ' SEGUNDOS', 300, 50);
 
+ if (floor(millis() / 1000) >= 29) {
+  if (todasRecolectadas) {
+    this.estado = '3'; // victoria
+  } else {
+    this.estado = '4'; // derrota
+  }
+}
+
+    } else if (this.estado === '3') { //ganaste
+      noStroke();
+      image(imagen[2], 0, 0, 400, 400);
+      fill (0, 100);
+      rect (20, 20, 360, 360);
+      fill (255);
+      textSize (20);
+      text (this.textos [10], 150, 100);
+      text ( this.textos [11], 60, 140);
+            text ( this.textos [12], 150, 180);
+
+      textSize (12);
+      text (this.textos [13], 220, 380);
+    } else if (this.estado === '4') { //perdiste
+
+      image (imagen [3], 0, 0, 400, 400);
+      noStroke();
+      fill (0, 100);
+      rect (20, 20, 360, 360);
+      fill (255);
+      textSize (20);
+      text (this.textos[8], 30, 100);
+      text (this.textos[9], 50, 140);
+      textSize (12);
+      text (this.textos[13], 220, 380);
+    }
+  }
+  reiniciarJuego() {
+    this.laberinto = new Laberinto();
+    this.personaje = new Personaje();
+    this.estado = '0';
+    this.diarios = [
+      new Diario(3, 1, 30),
+      new Diario(5, 11, 30),
+      new Diario(9, 3, 30),
+      new Diario(5, 0, 30),
+      new Diario(9, 9, 30),
+    ];
+      this.tiempoInicio = millis();
+
+  }
 }
 function keyPressed () {
   if (keyCode === UP_ARROW) {
@@ -82,13 +128,15 @@ function keyPressed () {
   } else if (keyCode === RIGHT_ARROW) {
     juego.personaje.mover (1, 0);
   }
-  if (keyCode === ENTER && juego.estado === '0'){
-juego.estado = '1';
-  } else if (keyCode === ENTER && juego.estado === '1'){
+  if (keyCode === ENTER && juego.estado === '0') {
+    juego.estado = '1';
+  } else if (keyCode === ENTER && juego.estado === '1') {
     juego.estado='2';
-  }  else if (keyCode === ENTER && juego.estado === '3'){
+  } else if (keyCode === ENTER && juego.estado === '3') {
     juego.estado = '0';
-  } else if (keyCode === ENTER && juego.estado === '4'){
-  juego.estado = '0';
+  }   else if (juego.estado === '3' ) {
+    juego.reiniciarJuego();
+  } else if (  juego.estado === '4') {
+    juego.reiniciarJuego();
 }
 }
