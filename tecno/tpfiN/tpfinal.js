36de36bea9ac03f5481e5e7af5e06b36f8b1be8b
luizurieta <133244065+luizurieta@users.noreply.github.com>
2023-12-16 19:51:14 -0300

    Add files via upload

---
+++ tecno/tpfiN/tpfinal.js
@@ -0,0 +1,20 @@
+//tpfinal
+//Izurieta, Lucia y Giorello, Matias
+let presentacion;
+function setup() {
+  createCanvas(800, 800);
+  presentacion = new Presentacion();
+}
+
+function draw() {
+  background(255);
+  presentacion.mostrar();
+}
+
+function mousePressed() {
+  presentacion.clicpresentacion();
+}
+
+function keyPressed() {
+  presentacion.juego.teclas();
+}
